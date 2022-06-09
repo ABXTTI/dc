8e4906be30b9774148b46117744e6289b44e1943
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2022-06-09 05:18:56 +0500

    Add files via upload

--- ta_inventory/models/product_template_custom.py
+++ ta_inventory/models/product_template_custom.py
@@ -6,16 +6,20 @@ import datetime
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    # overriding existing field
+    weight = fields.Float(string="Weight", digits=(16,6))
+    volume =  fields.Float(string="Volume", digits=(16,6))
+
     mc_ean = fields.Char(string="MC EAN")
     mc_quantity = fields.Float(string="MC Quantity")
-    mc_weight = fields.Float(string="MC Weight in kg")
-    mc_volume = fields.Float(string="MC Volume Cubic Meter")
+    mc_weight = fields.Float(string="MC Weight in kg", digits=(16,6))
+    mc_volume = fields.Float(string="MC Volume Cubic Meter", digits=(16,6))
     mc_uom = fields.Many2one("uom.uom", string="MC UOM")
 
     sc_ean = fields.Char(string="SC EAN")
     sc_quantity = fields.Float(string="SC Quantity")
-    sc_weight = fields.Float(string="SC Weight in kg")
-    sc_volume = fields.Float(string="SC Volume Cubic Meter")
+    sc_weight = fields.Float(string="SC Weight in kg", digits=(16,6))
+    sc_volume = fields.Float(string="SC Volume Cubic Meter", digits=(16,6))
     sc_uom = fields.Many2one("uom.uom", string="SC UOM")
 
     x_brand = fields.Many2one("ta.brand", string="Brand")
